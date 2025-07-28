d0d81ad14fedfc3bf5fcb50c924d154b751bc7b6
kavithma <kavithmadilmanie@gmail.com>
2025-07-28 09:13:52 +0530

    API changes

--- main.py
+++ main.py
@@ -10,7 +10,4 @@ def read_root():
 
 @app.get("/get-file")
 def get_file():
-    file_path = "sample.txt"
-    if os.path.exists(file_path):
-        return FileResponse(path=file_path, filename="sample.txt", media_type='text/plain')
-    return {"error": "File not found"}
+    return {"message": "This API works fine."}
